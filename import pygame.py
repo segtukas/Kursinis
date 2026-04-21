4f8ed4e40e14b2e6fd271995cb39c28c585bf743
segtukas <kajus.zeynalov@gmail.com>
2026-04-21 09:24:49 +0300

    Create import pygame.py

---
+++ import pygame.py
@@ -0,0 +1,14 @@
+import pygame
+
+pygame.init()
+
+screen = pygame.display.set_mode((800, 600))
+pygame.display.set_caption("Mano pirmas Pygame langas")
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+pygame.quit()
